d786f221422d221ed69554e5bf600ca4de2d3906
Yafik <mhmmdyfik@gmail.com>
2023-11-14 20:31:06 +0700

    update main.py

--- main.py
+++ main.py
@@ -4,38 +4,34 @@ import numpy as np
 import pandas as pd
 from tabulate import tabulate
 
-data = [
-    60, 64, 83, 92, 94, 80, 81, 59, 67, 80,
-    90, 73, 93, 71, 76, 79, 80, 78, 65, 82,
-    75, 84, 79, 84, 64, 81, 85, 90, 69, 59,
-    72, 87, 76, 87, 78, 72, 53, 60, 89, 87,
-    58, 92, 73, 91, 60, 65, 73, 67, 75, 64,
-]
+with open("data.txt", "r") as f:
+    data = [int(x) for x in f.read().split(",")]
 
-# data = [
-#     176, 153, 158, 144, 169, 144, 152, 148,
-#     168, 166, 154, 160, 168, 174, 168, 149,
-#     166, 170, 170, 171, 161, 174, 161, 167,
-#     171, 173, 155, 168, 144, 171, 152, 173,
-#     147, 168, 148, 173, 144, 146, 155, 168,
-#     141, 176, 156, 156, 167, 176, 152, 174,
-#     162, 155, 143, 139, 154, 143, 167, 151,
-#     155, 175, 154, 153, 162, 156, 174, 154,
-#     143, 162, 146, 168, 172, 162, 157, 147,
-#     173, 168, 161, 172, 159, 171, 152, 156
-# ]
+print("Data:")
+for i in range(0, len(data), 10):
+    print(*data[i:i+10])
+print()
+k_type = input("Pilih tipe pembulatan k (ceiling(c)/floor(f)): ")
+if k_type == "c":
+    k = math.ceil(1 + 3.322 * math.log10(len(data)))
+elif k_type == "f":
+    k = math.floor(1 + 3.322 * math.log10(len(data)))
+else:
+    print("Tipe pembulatan k tidak valid. Menggunakan ceiling sebagai default.")
+    k = math.ceil(1 + 3.322 * math.log10(len(data)))
+
+q = int(input("Letak kuartil keberapa yang ingin dicari? "))
+d = int(input("Letak desil keberapa yang ingin dicari? "))
+p = int(input("Letak persentil keberapa yang ingin dicari? \n"))
 
 n = len(data)
 r = max(data) - min(data)
-k_ceil = math.ceil(1 + 3.322 * math.log10(n))
-k_floor = math.floor(1 + 3.322 * math.log10(n))
-i = math.ceil(r / k_ceil)
+i = math.ceil(r / k)
 
 print("n = ", n)
 print("r = ", max(data), "-", min(data), "=", r, " ")
-print("k ceiling = 1 + 3.322 log 10(", n, ") = ", k_ceil)
-print("k floor = 1 + 3.322 log 10(", n, ") = ", k_floor)
-print("i = r/k = ", r, "/", k_ceil, "=", i, "\n")
+print(f"k {k_type} = 1 + 3.322 log 10({n}) = {k}")
+print("i = r/k = ", r, "/", k, "=", i, "\n")
 
 print("TDF: ")
 df = pd.DataFrame(
@@ -45,16 +41,33 @@ df = pd.DataFrame(
         "Frekuensi",
         "Frekuensi Relatif",
         "Frekuensi Relatif (%)",
+        "fixi",
+        "M",
+        "di",
+        "fidi",
+        "Fk"
     ]
 )
 
-for j in range(k_ceil):
+total_frekuensi = 0
+total_fixi = 0
+total_fidi = 0
+
+for j in range(k):
     batas_bawah = min(data) + i * j
     batas_atas = batas_bawah + i - 1
     titik_tengah = (batas_bawah + batas_atas) / 2
     frekuensi = len([x for x in data if batas_bawah <= x <= batas_atas])
     frekuensi_relatif = frekuensi / n
     frekuensi_relatif_persen = frekuensi_relatif * 100
+    fixi = frekuensi * titik_tengah
+    M = (min(data) + max(data)) / 2
+    di = titik_tengah - M
+    fidi = frekuensi * di
+    total_frekuensi += frekuensi
+    total_fixi += fixi
+    total_fidi += fidi
+    Fk = total_frekuensi
 
     df.loc[j] = [
         f"{batas_bawah}-{batas_atas}",
@@ -62,40 +75,35 @@ for j in range(k_ceil):
         frekuensi,
         frekuensi_relatif,
         frekuensi_relatif_persen,
+        fixi,
+        M,
+        di,
+        fidi,
+        Fk
     ]
 
-df["fixi"] = df["Frekuensi"] * df["Titik Tengah"]
-
-M = df.loc[k_ceil // 2, "Titik Tengah"]
-
-df["M"] = M
-df["di"] = df["Titik Tengah"] - M
-df["fidi"] = df["Frekuensi"] * df["di"]
-
-df["Fk"] = np.cumsum(df["Frekuensi"])
-
-df.loc[k_ceil] = [
+df.loc[k] = [
     "Σ",
     "",
-    df["Frekuensi"].sum(),
+    total_frekuensi,
     "",
-    df["Frekuensi Relatif (%)"].sum(),
-    df["fixi"].sum(),
+    total_frekuensi / n * 100,
+    total_fixi,
     "",
     "",
-    df["fidi"].sum(),
+    total_fidi,
     "",
 ]
 
-print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+print(tabulate(df, headers="keys", tablefmt="psql", showindex=False))
 print()
 
 print("Mean:")
-mean_fixi = df.loc[k_ceil, "fixi"] / n
-mean_fidi = M + df.loc[k_ceil, "fidi"] / n
+mean_fixi = df.loc[k, "fixi"] / n
+mean_fidi = M + df.loc[k, "fidi"] / n
 
-print(f"x̄ = {df.loc[k_ceil, 'fixi']}/{n} = {mean_fixi}")
-print(f"x̄ = {M} + ({df.loc[k_ceil, 'fidi']}/{n}) = {mean_fidi} \n")
+print(f"x̄ = {df.loc[k, 'fixi']}/{n} = {mean_fixi}")
+print(f"x̄ = {M} + ({df.loc[k, 'fidi']}/{n}) = {mean_fidi} \n")
 
 print("Modus:")
 max_freq = df.iloc[:-1]["Frekuensi"].max()
@@ -117,7 +125,7 @@ print()
 print("Median:")
 median_loc = n / 2
 median_kelas = ""
-cumulative_freq = 0 
+cumulative_freq = 0
 for index, row in df.iterrows():
     cumulative_freq += row["Frekuensi"]
     if cumulative_freq >= median_loc:
@@ -134,24 +142,38 @@ fm = median_frekuensi_kelas
 s_ = cumulative_freq - median_loc
 median = TBB + interval * (s / fm)
 
-print(tabulate([
-    [f"Letak Median = n/2 = {n}/2 = {median_loc} → terletak di kelas {median_kelas}"],
-    [f"Kelas Median = {median_kelas}"],
-    [f"TBB Kelas Median = {TBB} | TBA Kelas Median = {TBA}"],
-    [f"Frekuensi Kelas Median (fM) = {fm}"],
-    [f"Frek. Kumulatif sebelum Kelas Median = {prev_cumulative_freq} → s = {cumulative_freq} - {prev_cumulative_freq} = {s}"],
-    [f"Frek. Kumulatif sampai Kelas Median = {cumulative_freq} → s’ = {cumulative_freq} - {median_loc} = {s_}"],
-    [f"TBB Kelas Median + i(s/fm)\n"
-    f"= {TBB} + {interval}({s}/{fm:.1f})\n"
-    f"= {TBB} + {interval * (s / fm):.1f} = {median:.3f} ≈ {round(median, 1)}"],
-    [f"TBA Kelas Median - i(s'/fm)\n"
-    f"= {TBA} - {interval}({s_}/{fm:.1f})\n"
-    f"= {TBA} - {interval * (s_ / fm):.1f} = {median:.3f} ≈ {round(median, 1)}"]
-], tablefmt='rounded_grid'))
+print(
+    tabulate(
+        [
+            [
+                f"Letak Median = n/2 = {n}/2 = {median_loc} → terletak di kelas {median_kelas}"
+            ],
+            [f"Kelas Median = {median_kelas}"],
+            [f"TBB Kelas Median = {TBB} | TBA Kelas Median = {TBA}"],
+            [f"Frekuensi Kelas Median (fM) = {fm}"],
+            [
+                f"Frek. Kumulatif sebelum Kelas Median = {prev_cumulative_freq} → s = {cumulative_freq} - {prev_cumulative_freq} = {s}"
+            ],
+            [
+                f"Frek. Kumulatif sampai Kelas Median = {cumulative_freq} → s’ = {cumulative_freq} - {median_loc} = {s_}"
+            ],
+            [
+                f"TBB Kelas Median + i(s/fm)\n"
+                f"= {TBB} + {interval}({s}/{fm:.1f})\n"
+                f"= {TBB} + {interval * (s / fm):.1f} = {median:.3f} ≈ {round(median, 1)}"
+            ],
+            [
+                f"TBA Kelas Median - i(s'/fm)\n"
+                f"= {TBA} - {interval}({s_}/{fm:.1f})\n"
+                f"= {TBA} - {interval * (s_ / fm):.1f} = {median:.3f} ≈ {round(median, 1)}"
+            ],
+        ],
+        tablefmt="rounded_grid",
+    )
+)
 print()
 
 print("Kuartil ke - q:")
-q = 3
 letak_kuartil = q * n / 4
 cumulative_freq = 0
 kelas_kuartil = ""
@@ -171,24 +193,36 @@ fq = frekuensi_kelas_kuartil
 s_ = cumulative_freq - letak_kuartil
 kuartil = TBB + interval * (s / fq)
 
-print(tabulate([
-    [f"Letak Kuartil ke {q} = {q} x {n}/4 = {letak_kuartil}"],
-    [f"Kelas Kuartil ke {q} = {kelas_kuartil}"],
-    [f"TBB Kelas Kuartil ke {q} = {TBB} | TBA Kelas Kuartil ke {q} = {TBA}"],
-    [f"Frekuensi Kelas Kuartil ke {q} (fQ) = {fq}"],
-    [f"Frek. Kumulatif sebelum Kelas Kuartil ke {q} = {prev_cumulative_freq} → s = {letak_kuartil} - {prev_cumulative_freq} = {s}"],
-    [f"Frek. Kumulatif sampai Kelas Kuartil ke {q} = {cumulative_freq} → s’ = {cumulative_freq} - {letak_kuartil} = {s_}"],
-    [f"TBB Kelas Kuartil ke {q} + i(s/fq)\n"
-    f"= {TBB} + {interval}({s}/{fq:.1f})\n"
-    f"= {TBB} + {interval * (s / fq):.1f} = {kuartil:.2f}"],
-    [f"TBA Kelas Kuartil ke {q} - i(s'/fq)\n"
-    f"= {TBA} - {interval}({s_}/{fq:.1f})\n"
-    f"= {TBA} - {interval * (s_ / fq):.1f} = {kuartil:.2f}"]
-], tablefmt='rounded_grid'))
+print(
+    tabulate(
+        [
+            [f"Letak Kuartil ke {q} = {q} x {n}/4 = {letak_kuartil}"],
+            [f"Kelas Kuartil ke {q} = {kelas_kuartil}"],
+            [f"TBB Kelas Kuartil ke {q} = {TBB} | TBA Kelas Kuartil ke {q} = {TBA}"],
+            [f"Frekuensi Kelas Kuartil ke {q} (fQ) = {fq}"],
+            [
+                f"Frek. Kumulatif sebelum Kelas Kuartil ke {q} = {prev_cumulative_freq} → s = {letak_kuartil} - {prev_cumulative_freq} = {s}"
+            ],
+            [
+                f"Frek. Kumulatif sampai Kelas Kuartil ke {q} = {cumulative_freq} → s’ = {cumulative_freq} - {letak_kuartil} = {s_}"
+            ],
+            [
+                f"TBB Kelas Kuartil ke {q} + i(s/fq)\n"
+                f"= {TBB} + {interval}({s}/{fq:.1f})\n"
+                f"= {TBB} + {interval * (s / fq):.1f} = {kuartil:.2f}"
+            ],
+            [
+                f"TBA Kelas Kuartil ke {q} - i(s'/fq)\n"
+                f"= {TBA} - {interval}({s_}/{fq:.1f})\n"
+                f"= {TBA} - {interval * (s_ / fq):.1f} = {kuartil:.2f}"
+            ],
+        ],
+        tablefmt="rounded_grid",
+    )
+)
 print()
 
 print("Desil ke - d:")
-d = 7
 letak_desil = d * n / 10
 cumulative_freq = 0
 kelas_desil = ""
@@ -209,24 +243,36 @@ s_ = cumulative_freq - letak_desil
 desil = TBB + interval * (s / fd)
 desil = TBA - interval * (s_ / fd)
 
-print(tabulate([
-    [f"Letak Desil ke {d} = {d} x {n}/10 = {letak_desil}"],
-    [f"Kelas Desil ke {d} = {kelas_desil}"],
-    [f"TBB Kelas Desil ke {d} = {TBB} | TBA Kelas Desil ke {d} = {TBA}"],
-    [f"Frekuensi Kelas Desil ke {d} (fD) = {fd}"],
-    [f"Frek. Kumulatif sebelum Kelas Desil ke {d} = {prev_cumulative_freq} → s = {letak_desil} - {prev_cumulative_freq} = {s}"],
-    [f"Frek. Kumulatif sampai Kelas Desil ke {d} = {cumulative_freq} → s’ = {cumulative_freq} - {letak_desil} = {s_}"],
-    [f"TBB Kelas Desil ke {d} + i(s/fd)\n"
-    f"= {TBB} + {interval}({s}/{fd:.1f})\n"
-    f"= {TBB} + {interval * (s / fd):.1f} = {desil:.2f}"],
-    [f"TBA Kelas Desil ke {d} - i(s'/fd)\n"
-    f"= {TBA} - {interval}({s_}/{fd:.1f})\n"
-    f"= {TBA} - {interval * (s_ / fd):.1f} = {desil:.2f}"]
-], tablefmt='rounded_grid'))
+print(
+    tabulate(
+        [
+            [f"Letak Desil ke {d} = {d} x {n}/10 = {letak_desil}"],
+            [f"Kelas Desil ke {d} = {kelas_desil}"],
+            [f"TBB Kelas Desil ke {d} = {TBB} | TBA Kelas Desil ke {d} = {TBA}"],
+            [f"Frekuensi Kelas Desil ke {d} (fD) = {fd}"],
+            [
+                f"Frek. Kumulatif sebelum Kelas Desil ke {d} = {prev_cumulative_freq} → s = {letak_desil} - {prev_cumulative_freq} = {s}"
+            ],
+            [
+                f"Frek. Kumulatif sampai Kelas Desil ke {d} = {cumulative_freq} → s’ = {cumulative_freq} - {letak_desil} = {s_}"
+            ],
+            [
+                f"TBB Kelas Desil ke {d} + i(s/fd)\n"
+                f"= {TBB} + {interval}({s}/{fd:.1f})\n"
+                f"= {TBB} + {interval * (s / fd):.1f} = {desil:.2f}"
+            ],
+            [
+                f"TBA Kelas Desil ke {d} - i(s'/fd)\n"
+                f"= {TBA} - {interval}({s_}/{fd:.1f})\n"
+                f"= {TBA} - {interval * (s_ / fd):.1f} = {desil:.2f}"
+            ],
+        ],
+        tablefmt="rounded_grid",
+    )
+)
 print()
 
 print("Persentil ke - p:")
-p = 89
 letak_persentil = p * n / 100
 cumulative_freq = 0
 kelas_persentil = ""
@@ -247,18 +293,33 @@ s_ = cumulative_freq - letak_persentil
 persentil = TBB + interval * (s / fp)
 persentil = TBA - interval * (s_ / fp)
 
-print(tabulate([
-    [f"Letak Persentil ke {p} = {p} x {n}/100 = {letak_persentil}"],
-    [f"Kelas Persentil ke {p} = {kelas_persentil}"],
-    [f"TBB Kelas Persentil ke {p} = {TBB} | TBA Kelas Persentil ke {p} = {TBA}"],
-    [f"Frekuensi Kelas Persentil ke {p} (fP) = {fp}"],
-    [f"Frek. Kumulatif sebelum Kelas Persentil ke {p} = {prev_cumulative_freq} → s = {letak_persentil} - {prev_cumulative_freq} = {s}"],
-    [f"Frek. Kumulatif sampai Kelas Persentil ke {p} = {cumulative_freq} → s’ = {cumulative_freq} - {letak_persentil} = {s_}"],
-    [f"TBB Kelas Persentil ke {p} + i(s/fp)\n"
-    f"= {TBB} + {interval}({s}/{fp:.1f})\n"
-    f"= {TBB} + {interval * (s / fp):.1f} = {persentil:.2f}"],
-    [f"TBA Kelas Persentil ke {p} - i(s'/fp)\n"
-    f"= {TBA} - {interval}({s_}/{fp:.1f})\n"
-    f"= {TBA} - {interval * (s_ / fp):.1f} = {persentil:.2f}"]
-], tablefmt='rounded_grid'))
+print(
+    tabulate(
+        [
+            [f"Letak Persentil ke {p} = {p} x {n}/100 = {letak_persentil}"],
+            [f"Kelas Persentil ke {p} = {kelas_persentil}"],
+            [
+                f"TBB Kelas Persentil ke {p} = {TBB} | TBA Kelas Persentil ke {p} = {TBA}"
+            ],
+            [f"Frekuensi Kelas Persentil ke {p} (fP) = {fp}"],
+            [
+                f"Frek. Kumulatif sebelum Kelas Persentil ke {p} = {prev_cumulative_freq} → s = {letak_persentil} - {prev_cumulative_freq} = {s}"
+            ],
+            [
+                f"Frek. Kumulatif sampai Kelas Persentil ke {p} = {cumulative_freq} → s’ = {cumulative_freq} - {letak_persentil} = {s_}"
+            ],
+            [
+                f"TBB Kelas Persentil ke {p} + i(s/fp)\n"
+                f"= {TBB} + {interval}({s}/{fp:.1f})\n"
+                f"= {TBB} + {interval * (s / fp):.1f} = {persentil:.2f}"
+            ],
+            [
+                f"TBA Kelas Persentil ke {p} - i(s'/fp)\n"
+                f"= {TBA} - {interval}({s_}/{fp:.1f})\n"
+                f"= {TBA} - {interval * (s_ / fp):.1f} = {persentil:.2f}"
+            ],
+        ],
+        tablefmt="rounded_grid",
+    )
+)
 print()
